705637ea6ab335b7996f4815ea2f645984580fc7
sangeethsimon <simon@cerfacs.fr>
2024-06-13 19:00:29 +0200

    Default CTOR for PointStruct In this commit: -> We create a default constructor to allow other standard containers to construct these objects    on the fly.

--- symmetryFinder/include/Point.hpp
+++ symmetryFinder/include/Point.hpp
@@ -6,16 +6,18 @@
 #define epsilon  1e-10
 
 struct Point2D{
+    Point2D() = default;  // Default constructor
+
     //CTOR
     Point2D(int index, double x, double y ): m_Index(index), m_x(x), m_y(y) {}
 
     //Overload '==' for equality between points
     bool operator == (const Point2D& otherPoint)const{
      return (std::fabs(m_x - otherPoint.m_x) < epsilon && std::fabs(m_y - otherPoint.m_y) < epsilon);
-     }
+    }
     //Members
     int m_Index;
-    double m_x, m_y;
+    double m_x=0.; double m_y=0.;
 };
 
 #endif
