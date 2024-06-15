1bdd42d3060e20b617fd1c2897f0ef8a241963c8
sangeethsimon <simon@cerfacs.fr>
2024-06-15 19:02:58 +0200

    Improve tests for new hash funct

--- symmetryFinder/src/unitTesting.cpp
+++ symmetryFinder/src/unitTesting.cpp
@@ -6,25 +6,52 @@
 #include "Point.hpp"
 #include "Kernels.hpp"
 
-// Test for checking if the Hash function returns same key for exactly similar points
-TEST(Point2DHashFunctionTest, SimilarPointsHaveSameKey){
+// Test for checking if the hashFunc_float returns same key for exactly similar points
+TEST(Point2DHashFunctionTest, SimilarPointsHaveSameKey_hashFunc_float){
     Point2D point1(1, 1.0000000000, 2.0000000000);
     Point2D point2(2, 1.0000000000, 2.0000000000);
 
-    Kernels::hashFunc hasher;
+    Kernels::hashFunc_floatkey hasher;
     size_t hash1 = hasher(point1);
     size_t hash2 = hasher(point2);
 
     EXPECT_EQ(hash1, hash2);
 }
 
-// Test for checking if the Hash function returns different keys for dissimilar points
-TEST(Point2DHashFunctionTest, DissimilarPointsHaveDifferentKey){
+// Test for checking if the Hash function hashFunc_longlongintKey returns same key for exactly similar points
+TEST(Point2DHashFunctionTest, SimilarPointsHaveSameKey_hashFunc_longlongint){
+    Point2D point1(1, 1.0000000000, 2.0000000000);
+    Point2D point2(2, 1.0000000000, 2.0000000000);
+
+    Kernels::hashFunc_longlongintkey hasher;
+    size_t hash1 = hasher(point1);
+    size_t hash2 = hasher(point2);
+
+    EXPECT_EQ(hash1, hash2);
+}
+
+
+
+// Test for checking if the hashFunc_float returns different keys for dissimilar points
+TEST(Point2DHashFunctionTest, DissimilarPointsHaveDifferentKey_hashFunc_float){
 
     Point2D point1(1, 1.0, 2.0);
     Point2D point2(2, 3.0, 4.0);
 
-    Kernels::hashFunc hasher;
+    Kernels::hashFunc_floatkey hasher;
+    size_t hash1 = hasher(point1);
+    size_t hash2 = hasher(point2);
+
+    EXPECT_NE(hash1, hash2);
+}
+
+// Test for checking if the hashFunc_longlongint returns different keys for dissimilar points
+TEST(Point2DHashFunctionTest, DissimilarPointsHaveDifferentKey_hashFunc_longlongint){
+
+    Point2D point1(1, 1.0, 2.0);
+    Point2D point2(2, 3.0, 4.0);
+
+    Kernels::hashFunc_longlongintkey hasher;
     size_t hash1 = hasher(point1);
     size_t hash2 = hasher(point2);
 
@@ -58,8 +85,8 @@ TEST(Point2DCloudSymmetryTest, 2PointsSymmetryAboutAKnownSymmetryLine){
       {2, 1.0, 8.0},
     };
     double a = 2.; double b = -4.; double c = 15.;
-    bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
-
+    //bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
+    bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
     EXPECT_TRUE(isSetSymmetrical);
 }
 
@@ -73,7 +100,8 @@ TEST(Point2DCloudSymmetryTest, 3PointsNonSymmetryAboutALine){
       {3, 2.0, 9.0}
     };
     double a = 2.; double b = -4.; double c = 15.;
-    bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
+    //bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
+    bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
 
     EXPECT_FALSE(isSetSymmetrical);
 }
@@ -89,7 +117,8 @@ TEST(Point2DCloudSymmetryTest, duplicityType1InSymmetryAboutASymmetryLine){
       {2, 1.0, 8.0}
     };
     double a = 2.; double b = -4.; double c = 15.;
-    bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
+    //bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
+    bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
 
     EXPECT_TRUE(isSetSymmetrical);
 }
@@ -106,13 +135,41 @@ TEST(Point2DCloudSymmetryTest, duplicityType2InSymmetryAboutASymmetryLine){
       {2, 1.0, 8.0}
     };
     double a = 2.; double b = -4.; double c = 15.;
-    bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
+    //bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
+    bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
 
     EXPECT_TRUE(isSetSymmetrical);
 }
+//Test to check for symmetry of random set of points on a circle - intended to fail in its current config.
+TEST(LargePoint2DCloudSymmetryTest, LargePointCloudSymmetryAboutDiagonalOfACircle_randomPoints){
+    size_t N = 3;
+    double R = 1.;
+    bool isSetSymmetrical = true;
+    double a=0.; double b=0.; double c=0.;
 
-TEST(LargePoint2DCloudSymmetryTest, LargePointCloudSymmetryAboutDiagonalOfACircle){
-    size_t N = 10;
+    std::vector<Point2D> points;
+    points.reserve(N); // Reserve space for N points
+
+    const double PI = 3.14159265358979323846;
+    std::srand(std::time(0));
+
+    for (int i = 0; i < N; ++i) {
+        //double theta = 2.0 * PI * i / N; // Angle in radians
+        double x = static_cast<double>(std::rand()) / RAND_MAX - 0.5;
+        double y = sqrt(R*R - x*x);
+        points.emplace_back(i, x, y); // Create a Point2D and add to vector
+    }
+
+    isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
+
+    EXPECT_FALSE(isSetSymmetrical);
+}
+
+
+//Test to check for symmetry of a curated set of points on a circle - intended to pass.
+//This test also features points lying on the symmetry line and serve as a check on the quality of hashing
+TEST(LargePoint2DCloudSymmetryTest, LargePointCloudSymmetryAboutDiagonalOfACircle_symmetricalPoints){
+    size_t N = 2;
     double R = 1.0;
     bool isSetSymmetrical = true;
     double a=0.; double b=0.; double c=0.;
@@ -129,8 +186,34 @@ TEST(LargePoint2DCloudSymmetryTest, LargePointCloudSymmetryAboutDiagonalOfACircl
     }
     isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
 
-    EXPECT_FALSE(isSetSymmetrical);
+    EXPECT_TRUE(isSetSymmetrical);
 }
+
+//Test to check for symmetry of random set of points on a unit square centered at 0,0 about its horizontal axis
+TEST(LargePoint2DCloudSymmetryTest, LargePointCloudOnAnUnitSquareSymmetryAboutHorizontalAxis){
+    size_t N = 5000;
+    bool isSetSymmetrical = true;
+    double a=0.; double b=0.; double c=0.;
+
+    std::vector<Point2D> points;
+    points.reserve(N); // Reserve space for N points
+
+    std::srand(std::time(0));
+
+    for (size_t i = 0; i < N / 2; ++i) {
+        double x = static_cast<double>(std::rand()) / RAND_MAX - 0.5; // Random x in [-0.5, 0.5]
+        double y = static_cast<double>(std::rand()) / RAND_MAX / 2 - 0.25; // Random y in [-0.25, 0.25)
+
+        // Add point and its reflection
+        points.emplace_back(i, x, y);
+        points.emplace_back(-i, x, -y);
+    }
+    isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
+
+    EXPECT_TRUE(isSetSymmetrical);
+}
+
+
 
 //Driver to initiate tests
 int main(int argc, char** argv){
