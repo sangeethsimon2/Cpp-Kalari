a20d79e67d1714e776931d29214eb45ff97ee6fb
sangeethsimon <simon@cerfacs.fr>
2024-06-20 12:36:51 +0200

    Modfy unit tests and perform timing comparisons In this commit: - We replace a few unit tests to suite the current algorithm - We introduce 1 new test for easy logic check - We perform timing comparisons of the new algo on the same problems as previous algos

--- symmetryFinder/src/unitTesting.cpp
+++ symmetryFinder/src/unitTesting.cpp
@@ -8,6 +8,7 @@
 #include "Point.hpp"
 #include "Kernels.hpp"
 
+//#########################################[HASH FUNCTION TEST SUITE]################
 // Test for checking if the hashFunc_float returns same key for exactly similar points
 TEST(Point2DHashFunctionTest, SimilarPointsHaveSameKey_hashFunc_float){
     Point2D point1(1, 1.0000000000, 2.0000000000);
@@ -60,6 +61,10 @@ TEST(Point2DHashFunctionTest, DissimilarPointsHaveDifferentKey_hashFunc_longlong
     EXPECT_NE(hash1, hash2);
 }
 
+//##################################################################################################
+
+//#########################################[REFLECTION METHODS TEST SUITE]################
+
 // Test for checking if the reflection code's accuracy for reflection about x axis
 TEST(Point2DReflectionTest, ReflectionOnXAxis) {
     Point2D point(1, 1.0, 2.0);
@@ -78,22 +83,54 @@ TEST(Point2DReflectionTest, ReflectionOnYAxis) {
     EXPECT_EQ(reflectedPoint, expectedReflectedPoint);
 }
 
+//##################################################################################################
+
+//#########################################[SMALL POINT CLOUD SYMMETRY TEST SUITE]################
+
 //Test for checking if a known pair of points are symmetrical wrt a known symmetry line
-TEST(Point2DCloudSymmetryTest, 2PointsSymmetryAboutAKnownSymmetryLine){
+TEST(SmallPoint2DCloudSymmetryTest, 2PointsSymmetryAboutXaxis){
+
+    //Define the point cloud on a plane
+    std::vector<Point2D> points{
+      {1, 1.0, 1.0},
+      {2, 1.0, -1.0},
+    };
+    double a = 0.; double b = -1.; double c = 0.;
+    bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
+    EXPECT_TRUE(isSetSymmetrical);
+}
+
+//Test for checking if a known pair of points are symmetrical wrt a known symmetry line
+TEST(SmallPoint2DCloudSymmetryTest, 2PointsSymmetryAboutAKnownSymmetryLine){
 
     //Define the point cloud on a plane
     std::vector<Point2D> points{
       {1, 4.0, 2.0},
       {2, 1.0, 8.0},
     };
-    double a = 2.; double b = -4.; double c = 15.;
-    //bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
+    double a = 0.447214; double b = -0.894427; double c = 3.3541;
     bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
     EXPECT_TRUE(isSetSymmetrical);
 }
 
+//Test to check for symmetry within a point cloud containing duplicate points about a known symmetry line
+TEST(SmallPoint2DCloudSymmetryTest, duplicityType1InSymmetryAboutAKnownSymmetryLine){
+
+    //Define the point cloud on a plane
+    std::vector<Point2D> points{
+      {1, 4.0, 2.0},
+      {2, 1.0, 8.0},
+      {2, 1.0, 8.0},
+    };
+    double a = 0.447214; double b = -0.894427; double c = 3.3541;
+    bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
+
+    EXPECT_TRUE(isSetSymmetrical);
+}
+
+
 //Test for checking if a known group of points are NOT symmetrical wrt a line
-TEST(Point2DCloudSymmetryTest, 3PointsNonSymmetryAboutALine){
+TEST(SmallPoint2DCloudSymmetryTest, 3PointsNonSymmetryAboutALine){
 
     //Define the point cloud on a plane
     std::vector<Point2D> points{
@@ -102,31 +139,14 @@ TEST(Point2DCloudSymmetryTest, 3PointsNonSymmetryAboutALine){
       {3, 2.0, 9.0}
     };
     double a = 2.; double b = -4.; double c = 15.;
-    //bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
     bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
 
     EXPECT_FALSE(isSetSymmetrical);
 }
 
-//Test to check for symmetry within a point cloud containing duplicate points about a known symmetry line
-TEST(Point2DCloudSymmetryTest, duplicityType1InSymmetryAboutASymmetryLine){
-
-    //Define the point cloud on a plane
-    std::vector<Point2D> points{
-      {1, 4.0, 2.0},
-      {2, 1.0, 8.0},
-      {2, 1.0, 8.0},
-      {2, 1.0, 8.0}
-    };
-    double a = 2.; double b = -4.; double c = 15.;
-    //bool isSetSymmetrical = Kernels::IsSymmetricSet(points, a, b, c);
-    bool isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
-
-    EXPECT_TRUE(isSetSymmetrical);
-}
 
 //Test to check for symmetry within a point cloud containing duplicate points about a known symmetry line
-TEST(Point2DCloudSymmetryTest, duplicityType2InSymmetryAboutASymmetryLine){
+TEST(SmallPoint2DCloudSymmetryTest, duplicityType2InSymmetryAboutAKnownSymmetryLine){
 
     //Define the point cloud on a plane
     std::vector<Point2D> points{
@@ -142,31 +162,9 @@ TEST(Point2DCloudSymmetryTest, duplicityType2InSymmetryAboutASymmetryLine){
 
     EXPECT_TRUE(isSetSymmetrical);
 }
-//Test to check for symmetry of random set of points on a circle - intended to fail in its current config.
-TEST(LargePoint2DCloudSymmetryTest, LargePointCloudSymmetryAboutDiagonalOfACircle_randomPoints){
-    size_t N = 1;
-    double R = 1.;
-    bool isSetSymmetrical = true;
-    double a=0.; double b=0.; double c=0.;
+//##################################################################################################
 
-    std::vector<Point2D> points;
-    points.reserve(N); // Reserve space for N points
-
-    const double PI = 3.14159265358979323846;
-    std::srand(std::time(0));
-
-    for (int i = 0; i < N; ++i) {
-        //double theta = 2.0 * PI * i / N; // Angle in radians
-        double x = static_cast<double>(std::rand()) / RAND_MAX - 0.5;
-        double y = sqrt(R*R - x*x);
-        points.emplace_back(i, x, y); // Create a Point2D and add to vector
-    }
-
-    isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
-
-    EXPECT_FALSE(isSetSymmetrical);
-}
-
+//#########################################[LARGE POINT CLOUD SYMMETRY TEST SUITE]################
 
 //Test to check for symmetry of a curated set of points on a circle - intended to pass.
 //This test also features points lying on the symmetry line and serve as a check on the quality of hashing
@@ -191,9 +189,34 @@ TEST(LargePoint2DCloudSymmetryTest, LargePointCloudSymmetryAboutDiagonalOfACircl
     EXPECT_TRUE(isSetSymmetrical);
 }
 
+//Test to check for symmetry of random set of points on a circle - intended to fail in its current config.
+TEST(LargePoint2DCloudSymmetryTest, LargePointCloudSymmetryAboutDiagonalOfACircle_randomPoints){
+    size_t N = 3;
+    double R = 1.;
+    bool isSetSymmetrical = true;
+    double a=0.; double b=0.; double c=0.;
+
+    std::vector<Point2D> points;
+    points.reserve(N); // Reserve space for N points
+
+    const double PI = 3.14159265358979323846;
+    std::srand(std::time(0));
+
+    for (int i = 0; i < N; ++i) {
+        //double theta = 2.0 * PI * i / N; // Angle in radians
+        double x = static_cast<double>(std::rand()) / RAND_MAX - 0.5;
+        double y = sqrt(R*R - x*x);
+        points.emplace_back(i, x, y); // Create a Point2D and add to vector
+    }
+
+    isSetSymmetrical = Kernels::checkForSymmetryInPointCloud(points, a, b, c);
+
+    EXPECT_FALSE(isSetSymmetrical);
+}
+
 //Test to check for symmetry of random set of points on a unit square centered at 0,0 about its horizontal axis
 TEST(LargePoint2DCloudSymmetryTest, LargePointCloudOnAnUnitSquareSymmetryAboutHorizontalAxis){
-    size_t N = 2;
+    size_t N = 5000;
     bool isSetSymmetrical = true;
     double a=0.; double b=0.; double c=0.;
 
@@ -214,9 +237,7 @@ TEST(LargePoint2DCloudSymmetryTest, LargePointCloudOnAnUnitSquareSymmetryAboutHo
 
     EXPECT_TRUE(isSetSymmetrical);
 }
-
-
-
+//##################################################################################################
 
 //Driver to initiate tests
 int main(int argc, char** argv){
